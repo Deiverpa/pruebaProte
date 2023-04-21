eedbfc7a63c1dc3e07dd4148af4553e48cc1f278
Deiverpa <deipa280@hotmail.com>
2023-04-21 18:07:28 -0500

    mejora de lógica para traer y consumir superheroes
    ciclo for

--- pruebaprote/src/Components/Apis/Superheroe.jsx
+++ pruebaprote/src/Components/Apis/Superheroe.jsx
@@ -2,7 +2,16 @@ import React, { useState, useEffect } from "react";
 import "./Superheroe.css";
 
 function Superheroe() {
-  const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // setear cantidad de superheroes
+
+  const superheroqty = 20;
+  const ids = [];
+
+  for (let index = 1; index < superheroqty; index++) {
+    ids.push([index]);
+  }
+
+  // const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [datos, setDatos] = useState([]);
 
   useEffect(() => {
